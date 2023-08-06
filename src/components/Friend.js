773334c645c6f5a76d722c57891e36c7a827c671
Jamie-Ellen Weems <jamie-ellen@Matts-MacBook-Air.local>
2023-08-06 13:21:37 -0700

    will need to redo for review

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,12 +1,59 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components'
 
-export default function Friend({ info, action }) {
+// .friend {
+//   width: 60%;
+//   display: flex;
+//   justify-content: space-between;
+// }
+const kf = keyframes
+` 50%{
+    transform: scale(0.8);
+  }
+  100% {
+    transform: scale(1);
+    opacity: 1;
+  }
+`
+const StyledFriend = styled.div
+` width: 60%;
+  display: flex;
+  justify-content: space-between;
+  padding: 4px;
+  border-bottom: 4px solid black;
+
+  background-color: ${pr => pr.theme.primaryColor};
+  color: ${pr => pr.theme.white};
+
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background-color: ${pr => pr.theme.secondaryColor};
+  }
+
+  &::before {
+    content:'${pr => pr.bestie ? 'Bestie!!':'Dear Friend!'}'
+  }
+
+  button {
+    background-color: ${pr => pr.theme.tertiaryColor};
+    &:hover {
+      transform: scale(1.2);
+    }
+  }
+
+  transform: scale(2); //STARTING THE ANIMATION
+  opacity: 0;
+  animation: ${kf} 0.3s ease-in-out forwards;
+`
+
+export default function Friend({ info, action, bestie }) {
   return (
-    <div className='friend'>
+    <StyledFriend bestie={bestie}>
       {info.name}
       <button onClick={() => action(info.id)}>
         See details
       </button>
-    </div>
+    </StyledFriend>
   )
 }
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,1 +1,9 @@
 // theme object here
+export default {
+      primaryColor: 'forestgreen',
+      secondaryColor: 'yellow',
+      tertiaryColor: 'brickred',
+      black: 'black',
+      white: 'white',
+      breakpointMobile: '(max-width: 550px)'
+}
